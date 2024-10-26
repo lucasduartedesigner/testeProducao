f61201243dcd008224aebe5e11c25198935f53ee
Hugo <hgmello25@gmail.com>
2024-10-26 14:32:52 -0300

    Cadastro problema

---
+++ php/cadastra/problema.php
@@ -0,0 +1,56 @@
+<?php
+session_start();
+require '../../conn/conn.php'; // Conexão com o banco de dados
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $nome = $_POST['nome'];
+    $disparador = $_POST['disparador'];
+    $identificacao = $_POST['identificacao'];
+    $desc_hda = $_POST['desc_hda'];
+    $desc_hpp = $_POST['desc_hpp'];
+    $desc_hs = $_POST['desc_hs'];
+    $desc_hpf = $_POST['desc_hpf'];
+    $arquivo = $_POST['arquivo'];
+    $diagnostico = $_POST['diagnostico'];
+
+    // Verifica se todos os campos obrigatórios foram preenchidos
+    if (!empty($nome) && !empty($disparador) && !empty($identificacao) && !empty($desc_hda) && !empty($desc_hpp) 
+        && !empty($desc_hs) && !empty($desc_hpf) && !empty($arquivo) && !empty($diagnostico)) {
+
+        // Verifica se o registro já existe com base em um campo específico
+        $sql = "SELECT * FROM problema WHERE nome = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param("s", $nome);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        if ($result->num_rows > 0) {
+            // Usuário já existente
+            $_SESSION['msg'] = "Problema escolhido já existente";
+            header("Location: ../../problema.php"); // Redireciona para o problema
+            exit();        
+        } else {
+            // Prepara a inserção no banco com os novos campos
+            $sql = "INSERT INTO problema (nome, disparador, identificacao, desc_hda, desc_hpp, desc_hs, desc_hpf, arquivo, diagnostico) 
+                    VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            $stmt = $conn->prepare($sql);
+            $stmt->bind_param("sssssssss", $nome, $disparador, $identificacao, $desc_hda, $desc_hpp, $desc_hs, $desc_hpf, $arquivo, $diagnostico);
+
+            // Executa e verifica se a inserção foi bem-sucedida
+            if ($stmt->execute()) {
+                $_SESSION['msg'] = "Cadastro realizado com sucesso!";
+                header("Location: ../../problema.php"); // Redireciona para o problema
+                exit();  
+            } else {
+                $_SESSION['msg'] = "Erro ao registrar usuário: " . $stmt->error;
+                header("Location: ../../problema.php"); // Redireciona para o problema
+                exit(); 
+            }
+        }
+    } else {
+        $_SESSION['msg'] = "Todos os campos são obrigatórios!";
+        header("Location: ../../cadastro.php"); // Redireciona para o cadastro
+        exit();
+    }
+}
+?>
---
+++ problema.php
@@ -0,0 +1,72 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <h2>Login</h2>
+
+    <?php if (isset($error)): ?>
+        <p style="color: red;"><?php echo $error; ?></p>
+    <?php endif; ?>
+
+    <form action="php/cadastra/problema.php" method="POST">
+        
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" required>
+        <br>
+
+        <label for="disparador">Disparador:</label>
+        <input type="text" name="disparador" id="disparador" required>
+        <br>
+
+        <label for="identificacao">Identificacao:</label>
+        <input type="text" name="identificacao" id="identificacao" required>
+        <br>
+
+        <label for="desc_hda">HDA:</label>
+        <input type="text" name="desc_hda" id="desc_hda" required>
+        <br>
+
+        <label for="desc_hpp">HPP:</label>
+        <input type="text" name="desc_hpp" id="desc_hpp" required>
+        <br>
+
+        <label for="desc_hs">HS:</label>
+        <input type="text" name="desc_hs" id="desc_hs" required>
+        <br>
+
+        <label for="desc_hpf">HPF:</label>
+        <input type="text" name="desc_hpf" id="desc_hpf" required>
+        <br>
+
+        <label for="arquivo">Arquivo:</label>
+        <input type="text" name="arquivo" id="arquivo" required>
+        <br>
+
+        <label for="diagnostico">Diagnostico:</label>
+        <input type="text" name="diagnostico" id="diagnostico" required>
+        <br>
+
+        <button type="submit">Salvar</button>
+
+        <?php if (isset($_SESSION['msg'])): ?>
+            <p style="color: red;"><?php echo $_SESSION['msg']; $_SESSION['msg']= "" ?></p>
+        <?php endif; ?>
+
+    </form>
+
+</body>
+
+</html>
