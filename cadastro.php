9d3684f88fb15347691900964045bde3f5e1e420
Hugo <hgmello25@gmail.com>
2024-10-26 14:35:38 -0300

    atualização

--- cadastro.php
+++ cadastro.php
@@ -10,12 +10,12 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
+    <title>Cadastro</title>
 </head>
 
 <body>
 
-    <h2>Login</h2>
+    <h2>Cadastro</h2>
 
     <?php if (isset($error)): ?>
         <p style="color: red;"><?php echo $error; ?></p>
