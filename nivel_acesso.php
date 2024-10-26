286d438e53ad52bb85dd7579cb27d182476e513f
Hugo <hgmello25@gmail.com>
2024-10-26 13:43:19 -0300

    Nivel acesso

---
+++ nivel_acesso.php
@@ -0,0 +1,40 @@
+<?php
+
+session_start();
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+
+<body>
+
+    <h2>Login</h2>
+
+    <?php if (isset($error)): ?>
+        <p style="color: red;"><?php echo $error; ?></p>
+    <?php endif; ?>
+
+    <form action="php/cadastra/nivel_acesso.php" method="POST">
+        
+        <label for="nome">Nome:</label>
+        <input type="text" name="nome" id="nome" required>
+        <br>
+
+        <button type="submit">Confirma</button>
+
+        <?php if (isset($_SESSION['msg'])): ?>
+            <p style="color: red;"><?php echo $_SESSION['msg']; $_SESSION['msg']= "" ?></p>
+        <?php endif; ?>
+
+    </form>
+
+</body>
+
+</html>
--- php/cadastra/nivel_acesso.php
+++ php/cadastra/nivel_acesso.php
@@ -6,42 +6,40 @@ require '../../conn/conn.php'; // conexão com o banco de dados
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $nome = $_POST['nome'];
-    $senha = $_POST['senha'];
-    $cpf = $_POST['cpf'];
-    $email = $_POST['email'];
-    $matricula = $_POST['matricula'];
+    $status = 1;
+
 
     // Verifica se todos os campos obrigatórios foram preenchidos
-    if (!empty($nome) && !empty($senha) && !empty($cpf) && !empty($email) && !empty($matricula)) {
+    if (!empty($nome)) {
         // Criptografa a senha com MD5 para comparação
         $senhaMd5 = md5($senha);
 
         // Consulta SQL para verificar o usuário e senha
-        $sql = "SELECT * FROM pessoa WHERE matricula = ? AND senha = ?";
+        $sql = "SELECT * FROM pessoa WHERE nome = ?";
         $stmt = $conn->prepare($sql);
-        $stmt->bind_param("ss", $matricula, $senhaMd5);
+        $stmt->bind_param("s", $nome);
         $stmt->execute();
         $result = $stmt->get_result();
 
         if ($result->num_rows > 0) {
             // Login bem-sucedido
-            $_SESSION['msg'] = "Usuário já existente";
-            header("Location: ../../cadastro.php"); // Redireciona para o cadastro
+            $_SESSION['msg'] = "Nivel de acesso já existente";
+            header("Location: ../../nivel_acesso.php"); // Redireciona para o nivel_acesso
             exit();        
         } else {
             // Prepara a inserção no banco
-            $sql = "INSERT INTO pessoa (nome, senha, cpf, email, matricula) VALUES (?, ?, ?, ?, ?)";
+            $sql = "INSERT INTO nivel_acesso (nome, status) VALUES (?, ?)";
             $stmt = $conn->prepare($sql);
-            $stmt->bind_param("sssss", $nome, $senhaMd5, $cpf, $email, $matricula);
+            $stmt->bind_param("si", $nome, $status);
             // Executa e verifica se a inserção foi bem-sucedida
 
             if ($stmt->execute()) {
             $_SESSION['msg'] = "Cadastro realizado com sucesso!";
-            header("Location: ../../cadastro.php"); // Redireciona para o cadastro
+            header("Location: ../../nivel_acesso.php"); // Redireciona para o nivel_acesso
             exit();  
             } else {
             $_SESSION['msg'] = "Erro ao registrar usuário:";
-            header("Location: ../../cadastro.php"); // Redireciona para o cadastro
+            header("Location: ../../nivel_acesso.php"); // Redireciona para o nivel_acesso
             exit(); 
             }
         }
