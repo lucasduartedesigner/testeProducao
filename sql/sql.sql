6f3ef750fd18d5c43ad1a3e3c9ba73def1474ce1
Lucas Duarte <57960201+lucasduartedesigner@users.noreply.github.com>
2024-10-26 11:33:30 -0300

    Update sql.sql

--- sql/sql.sql
+++ sql/sql.sql
@@ -1,3 +1,4 @@
+-- Tabela criada para configuração do sistema
 CREATE TABLE IF NOT EXISTS `config` (
   `id_config` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -21,6 +22,7 @@ CREATE TABLE IF NOT EXISTS `config` (
   PRIMARY KEY (`id_config`)
 );
 
+-- Tabela para criação de niveis de acesso
 CREATE TABLE IF NOT EXISTS `nivel_acesso` (
   `id_nivel_acesso` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -30,8 +32,9 @@ CREATE TABLE IF NOT EXISTS `nivel_acesso` (
   `user_updated` int(11) DEFAULT NULL,
   `dt_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_nivel_acesso`)
-)
+);
 
+-- Tabela criada para cadastro de pessoas 
 CREATE TABLE IF NOT EXISTS `pessoa` (
   `id_pessoa` int(11) NOT NULL AUTO_INCREMENT,
   `id_nivel_acesso` int(11) NOT NULL,
@@ -52,10 +55,11 @@ CREATE TABLE IF NOT EXISTS `pessoa` (
   KEY `fk_professor_nivel_acesso` (`id_nivel_acesso`)
 );
 
+-- Tabela criada para vincular pessoa/estudante a uma turma
 CREATE TABLE IF NOT EXISTS `estudante_turma` (
   `id_estudante_turma` int(11) NOT NULL AUTO_INCREMENT,
   `id_pessoa` int(11) NOT NULL,
-  `status` int(11) DEFAULT NULL,
+  `cod_status` int(11) DEFAULT NULL,
   `codcurso` int(11) DEFAULT NULL,
   `periodo` int(11) DEFAULT NULL,
   `semestre` varchar(6) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -68,6 +72,7 @@ CREATE TABLE IF NOT EXISTS `estudante_turma` (
   PRIMARY KEY (`id_estudante_turma`)
 );
 
+-- Tabela criada para montagem do menu
 CREATE TABLE IF NOT EXISTS `menu` (
   `id_menu` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
@@ -81,8 +86,9 @@ CREATE TABLE IF NOT EXISTS `menu` (
   `user_updated` int(11) DEFAULT NULL,
   `dt_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id_menu`)
-)
+);
 
+-- Tabela criada para vincular nivel de acesso e permissão aos menus e paginas
 CREATE TABLE IF NOT EXISTS `menu_acesso` (
   `id_menu_acesso` int(11) NOT NULL AUTO_INCREMENT,
   `id_nivel_acesso` int(11) NOT NULL,
@@ -97,4 +103,24 @@ CREATE TABLE IF NOT EXISTS `menu_acesso` (
   PRIMARY KEY (`id_menu_acesso`),
   KEY `fk_menu_acesso_nivel_acesso` (`id_nivel_acesso`),
   KEY `fk_menu_acesso_menu` (`id_menu`)
-)
+);
+
+-- Tabela criada para criacao da situacao problema
+CREATE TABLE IF NOT EXISTS `problema` (
+  `id_problema` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `disparador` varchar(5000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `identificacao` varchar(5000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `desc_hda` varchar(5000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `desc_hpp` varchar(5000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `desc_hs` varchar(5000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `desc_hpf` varchar(5000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `arquivo` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `diagnostico` varchar(1000) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `cod_status` int(11) DEFAULT NULL,
+  `user_created` int(11) DEFAULT NULL,
+  `dt_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user_updated` int(11) DEFAULT NULL,
+  `dt_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id_problema`)
+);
